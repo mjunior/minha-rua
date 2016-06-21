bf34fd44dbb3e1e3a49ca4b79b58f80b9e3db17c
Mauricio Jr <mauriciojs.junior@gmail.com>
2016-06-21 00:22:03 -0300

    Adicionado tela de home

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,5 @@
+class HomeController < ApplicationController
+  def index
+  	@nomes = ["Mauricio","Jose","Da","Silva","Junior"];
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
+
+  match 'home' => 'home#index', via: 'get'
+  match 'inicio' => 'home#index', via: 'get'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
