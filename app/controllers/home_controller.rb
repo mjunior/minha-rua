d2d1dcd7ab242a87415c8f634da3964cda7ceda1
Mauricio Jr <mauriciojs.junior@gmail.com>
2016-07-18 16:07:35 -0300

    Pagina home novas areas

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,11 +2,13 @@ class HomeController < ApplicationController
   def index
 
   	@complaintList = Complaint.all
-
+  	@lastest = Complaint.last(10).reverse
+  	@topList = Complaint.order(likes: :desc)
   	if user_signed_in?
   		@logado = true
   	else
   		@logado = false
   	end
+
   end
 end
