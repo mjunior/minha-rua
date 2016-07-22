68996bc157f33abef2b36e24115ed38a8ebff3c9
Mauricio Jr <mauriciojs.junior@gmail.com>
2016-07-22 12:43:33 -0300

    Removendo arquivos antigos

--- db/migrate/20160623152621_remove_password_from_user.rb
+++
@@ -1,5 +0,0 @@
-class RemovePasswordFromUser < ActiveRecord::Migration
-  def change
-    remove_column :users, :password
-  end
-end
--- db/migrate/20160623153004_edit_first_name_fromuser.rb
+++
@@ -1,7 +0,0 @@
-class EditFirstNameFromuser < ActiveRecord::Migration
-  def change
-  	change_column :users, :first_name, :string, :null => true
-  	change_column :users, :last_name, :string, :null => true
-  	change_column :users, :bio, :string, :null => true
-  end
-end
