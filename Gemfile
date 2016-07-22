956b7557231abfb104862d9e338bfd7c57c8094a
Mauricio Jr <mauriciojs.junior@gmail.com>
2016-07-22 12:41:47 -0300

    Change to postgresql

--- Gemfile
+++ Gemfile
@@ -5,6 +5,9 @@ source 'https://rubygems.org'
 gem 'rails', '4.2.6'
 # Use mysql as the database for Active Record
 gem 'mysql2', '>= 0.3.13', '< 0.5'
+
+gem 'pg'
+
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
--- db/schema.rb
+++ db/schema.rb
@@ -13,50 +13,52 @@
 
 ActiveRecord::Schema.define(version: 20160720195956) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "categories", force: :cascade do |t|
-    t.string   "title",      limit: 255, null: false
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
-    t.string   "tag",        limit: 255
+    t.string   "title",      null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "tag"
   end
 
   create_table "complaints", force: :cascade do |t|
-    t.string   "People",      limit: 255
-    t.float    "latitude",    limit: 24
-    t.float    "longitude",   limit: 24
-    t.text     "body",        limit: 65535
-    t.string   "address",     limit: 255
-    t.string   "title",       limit: 255
-    t.string   "city",        limit: 255
-    t.integer  "likes",       limit: 4
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
-    t.integer  "user_id",     limit: 4
-    t.integer  "category_id", limit: 4
+    t.float    "latitude"
+    t.float    "longitude"
+    t.text     "body"
+    t.string   "address"
+    t.string   "title"
+    t.string   "city"
+    t.integer  "likes"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "user_id"
+    t.integer  "category_id"
   end
 
   add_index "complaints", ["category_id"], name: "index_complaints_on_category_id", using: :btree
   add_index "complaints", ["user_id"], name: "index_complaints_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  limit: 255,              null: false
-    t.string   "first_name",             limit: 255,              null: false
-    t.string   "last_name",              limit: 255
-    t.string   "bio",                    limit: 255
-    t.string   "facebook_url",           limit: 255
-    t.string   "twitter_url",            limit: 255
-    t.string   "instagram_url",          limit: 255
-    t.datetime "created_at",                                      null: false
-    t.datetime "updated_at",                                      null: false
-    t.string   "encrypted_password",     limit: 255, default: "", null: false
-    t.string   "reset_password_token",   limit: 255
+    t.string   "email",                               null: false
+    t.string   "first_name",                          null: false
+    t.string   "last_name"
+    t.string   "bio"
+    t.string   "facebook_url"
+    t.string   "twitter_url"
+    t.string   "instagram_url"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip",     limit: 255
-    t.string   "last_sign_in_ip",        limit: 255
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
