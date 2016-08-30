e4b02a9ee9e952374e385eed020be57c80201b12
Mauricio Jr <mauriciojs.junior@gmail.com>
2016-08-30 16:29:41 -0300

    Novas dimensoes foto

--- app/models/complaint.rb
+++ app/models/complaint.rb
@@ -2,7 +2,13 @@ class Complaint < ActiveRecord::Base
 	extend FriendlyId
 	friendly_id :title, use: :slugged
 
-	has_attached_file 	:image, styles: { medium: "500x500>",small:"200x200>", thumb: "100x100>" }, default_url: "/images/:style/missing.png",
+	has_attached_file 	:image, styles: { original: "1200x1200>",medium: "500x500>",small:"200x200>", thumb: "100x100>" },
+					 					convert_options: { 	small: 	"-quality 75 -strip",
+						 									medium: "-quality 75 -strip",
+						 									thumb: 	"-quality 75 -strip",
+                                     						original: "-quality 85 -strip" },
+
+						default_url: "/images/:style/missing.png",
 						:path => ':rails_root/public/images/complaints/:id-:basename-:style.:extension',
                   		:url => '/images/complaints/:id-:basename-:style.:extension'
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
