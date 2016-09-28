c85b0ae11636f6d20c5a732a73952146fd6de488
Mauricio Jr <mauriciojs.junior@gmail.com>
2016-09-28 11:57:16 -0300

    Bug cadastrar

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,7 +19,8 @@ class User < ActiveRecord::Base
 
   def self.new_with_session(params, session)
     super.tap do |user|
-      if data == session['devise.facebook_data'] && session['devise.facebook_data']['extra']['raw_info']
+      data = session['devise.facebook_data'] && session['devise.facebook_data']['extra']['raw_info']
+      if data
         user.email = data['email'] if user.email.blank?
       end
     end
